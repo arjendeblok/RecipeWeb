08e28cba7a39ad3be7487360756b45a72b45b363
arjendeblok <arjen@deblok.org>
2016-09-22 08:47:47 +0200

    Initial Web API
    Initial commit with a Web API that does nothing yet

---
+++ src/RecipeWeb/Controllers/RecipeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeWeb.Models;
+using System.Collections.Generic;
+
+namespace RecipeWeb.Controllers
+{
+  [Route("api/[controller]")]
+  public class RecipeController : Controller
+  {
+    [HttpGet]
+    public IEnumerable<Recipe> Get()
+    {
+      return new Recipe[0];
+    }
+
+    [HttpGet("{id}")]
+    public Recipe Get(int id)
+    {
+      return new Recipe { Id = id };
+    }
+
+    [HttpPost]
+    public int Post([FromBody]Recipe recipe)
+    {
+      return 1;
+    }
+
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody]Recipe recipe)
+    {
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
+}
---
+++ src/RecipeWeb/Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿namespace RecipeWeb.Models
+{
+  /// <summary>
+  /// A recipe
+  /// </summary>
+  public class Recipe
+  {
+    /// <summary>
+    /// identifier
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Title of the recipe
+    /// </summary>
+    public string Title { get; set; }
+  }
+}
